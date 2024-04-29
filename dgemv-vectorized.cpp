e1ecce12a49883d6600447f697b0f8ca37ca6cd0
gdhillonSFSU <gdhillon@sfsu.edu>
2024-04-28 18:08:24 -0700

    copied dgemv-basic into dgemv-vectorized

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,12 +1,19 @@
-const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
+const char *dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 
 /*
  * This routine performs a dgemv operation
  * Y :=  A * X + Y
- * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
- * On exit, A and X maintain their input values.
+ * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1
+ * vectors. On exit, A and X maintain their input values.
  */
-void my_dgemv(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of vectorized vector-matrix multiply
-
+void my_dgemv(int n, double *A, double *x, double *y) {
+  for (int i = 0; i < n; i++) {
+    double row_sum = 0;
+    for (int j = 0; j < n; j++) {
+      // multiply i * n to find the beginning index of the row we're on
+      // j is an offset of that row to find the correct index
+      row_sum += A[i * n + j] * x[j];
+    }
+    y[i] += row_sum;
+  }
 }
